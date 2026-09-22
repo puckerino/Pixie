4caa70bd50b8f2074ece3465247c56b037cf1707
puckerino <loregalafate+github@gmail.com>
2026-09-22 13:40:42 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+/* ========= CORE ========= */
+
+import "./core/pixiekit.js";
+import "./core/pixie-form-core.js";
+
+/* ========= MODULES ========= */
+
+import "./modules/pixie-copy.js";
+import "./modules/pixie-editor.js";
+import "./modules/pixie-fields.js";
+import "./modules/pixie-footer.js";
+import "./modules/pixie-navbar.js";
+import "./modules/pixie-form.js";
+import "./modules/pixie-reply-form.js";
+import "./modules/pixie-shop.js";
+import "./modules/pixie-theme.js";
+import "./modules/pixie-replace.js";
